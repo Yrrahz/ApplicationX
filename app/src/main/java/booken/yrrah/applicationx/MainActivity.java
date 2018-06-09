15e87bf180061f499dbe4d945a4d6ad8bc2d7e8b
Yrrahz <harry.dfh@gmail.com>
2018-06-09 20:56:24 +0200

    Menu added. Currently two options, Stats and About

--- app/src/main/java/booken/yrrah/applicationx/MainActivity.java
+++ app/src/main/java/booken/yrrah/applicationx/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -31,6 +34,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_stats:
+                Toast.makeText(getApplicationContext(),"Stats pressed!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_about:
+                Toast.makeText(getApplicationContext(),"About pressed!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void categoryButtonPressed(View view){
